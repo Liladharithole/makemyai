6ab156ef85e150f3b861835856d8904ad40e4fd1
LILADHAR <97004436+Liladharithole@users.noreply.github.com>
2025-08-02 23:46:46 +0530

    Add form submission and input handling to WriteArticle
    Introduced an onSubmitHandler for the article form and connected the input field to component state. Added a submit button with an icon to trigger article generation.

--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SparkleIcon } from "lucide-react";
+import { Edit, SparkleIcon } from "lucide-react";
 
 const WriteArticle = () => {
   const articleLength = [
@@ -11,16 +11,24 @@ const WriteArticle = () => {
   const [selectedLength, setSelectedLength] = useState(articleLength[0]);
   const [input, setInput] = useState("");
 
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    
+  }
+
+  
   return (
     <div className="h-auto overflow-y-scroll p-6 flex item-start flex-wrap gap-4 text-slate-700">
       {/* right col */}
-      <form className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
+      <form onSubmit={onSubmitHandler} className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200">
         <div className="flex items-center gap-3 mb-4">
           <SparkleIcon className="w-6 text-[var(--color-primary)]" />
           <h2 className="text-xl font-semibold">Article Configuration</h2>
         </div>
         <p>Article Topic</p>
         <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           type="text"
           className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
           placeholder="example: The Future of AI is ...."
@@ -42,9 +50,16 @@ const WriteArticle = () => {
             </span>
           ))}
         </div>
+        <br />
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[var(--color-primary)] to-[var(--color-secondary)] text-white px-4 py-2.5 rounded-full hover:scale-102 active:scale-95 transition-all duration-300 text-sm cursor-pointer">
+          <Edit className="w-5" />
+          Generate Article
+        </button>
       </form>
       {/* left col */}
-      <div></div>
+      <div>
+        
+      </div>
     </div>
   );
 };
